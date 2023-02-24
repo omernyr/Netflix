7abddec87de5dfdfb776fc18767e4258cc0305a0
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-02-25 00:43:09 +0300

    Added SearchResultsVC

---
+++ Netflix Clone/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix Clone
+//
+//  Created by macbook pro on 25.02.2023.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
