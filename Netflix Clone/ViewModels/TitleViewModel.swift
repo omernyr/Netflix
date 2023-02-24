b37d1cc2dd01b74bba73ca55e981353f66787f43
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-02-24 16:47:44 +0300

    Create ViewModel for convert to any title image download and showing with any title name in tableView

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by macbook pro on 23.02.2023.
+//
+
+import Foundation
+
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
