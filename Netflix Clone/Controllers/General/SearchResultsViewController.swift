a77c58776cd73d2c53a03c0b8fe58b79f99a328f
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-02-28 17:21:46 +0300

    Create func didSelectRowItemAt button tap end go to TitlePreviewVC

--- Netflix Clone/Controllers/General/SearchResultsViewController.swift
+++ Netflix Clone/Controllers/General/SearchResultsViewController.swift
@@ -7,9 +7,15 @@
 
 import UIKit
 
+protocol SearchResultsViewControllerDelegate: AnyObject {
+    func searchResultsViewControllerDidTapped(_ viewModel: TitlePreviewViewModel)
+}
+
 class SearchResultsViewController: UIViewController {
     
     public var titles: [Title] = [Title]()
+    
+    public weak var delegate: SearchResultsViewControllerDelegate?
     
     public let searchResultsCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -44,5 +50,22 @@ extension SearchResultsViewController: UICollectionViewDelegate, UICollectionVie
         let title = titles[indexPath.row]
         cell.configure(with: title.poster_path ?? "")
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
+        let title = titles[indexPath.row]
+        guard let titleName = title.original_title ?? title.original_name else { return }
+        API_Caller.shared.getMovie(with: titleName) { [weak self] results in
+            switch results {
+            case .success(let videoElement):
+                DispatchQueue.main.async {
+                    self?.delegate?.searchResultsViewControllerDidTapped(TitlePreviewViewModel(title: title.original_title ?? "", youtubeView: videoElement, titleOverView: title.overview ?? ""))
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
