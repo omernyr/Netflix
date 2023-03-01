0a6809a9ab625d02afb059e20232d6525e21c453
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-03-01 10:50:28 +0300

    ad tableView for download data and updated!

--- Netflix Clone/Controllers/Core/DownloadsViewController.swift
+++ Netflix Clone/Controllers/Core/DownloadsViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class DownloadsViewController: UIViewController {
     
+    private var titles: [TitleItem] = [TitleItem]()
     
+    private let downloadsTable: UITableView = {
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,5 +24,44 @@ class DownloadsViewController: UIViewController {
         title = "Downloads"
         navigationController?.navigationItem.largeTitleDisplayMode = .always
         navigationController?.navigationBar.prefersLargeTitles = true
+        view.addSubview(downloadsTable)
+        downloadsTable.delegate = self
+        downloadsTable.dataSource = self
+        fetchLocalStorageForDownload()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        downloadsTable.frame = view.bounds
+    }
+    
+    private func fetchLocalStorageForDownload() {
+        DataPersistenceManager.shared.fetchTitleFromDatabase { results in
+            switch results {
+            case .success(let titles):
+                self.titles = titles
+                self.downloadsTable.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
+extension DownloadsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else { return UITableViewCell() }
+        
+        cell.configure(with: TitleViewModel(titleName: titles[indexPath.row].original_name ?? titles[indexPath.row].original_title ?? "Unknown", posterURL: titles[indexPath.row].poster_path!))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
     }
 }
